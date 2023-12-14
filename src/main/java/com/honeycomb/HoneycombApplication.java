3d74aadec4d7af1466cfdf3f7dec7e2920bf2f58
Shreyansh Solanki <shreyansh1029@gmail.com>
2023-12-14 21:10:35 +0530

    initial commit

---
+++ src/main/java/com/honeycomb/HoneycombApplication.java
@@ -0,0 +1,13 @@
+package com.honeycomb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneycombApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneycombApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honeycomb/HoneycombApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honeycomb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneycombApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
